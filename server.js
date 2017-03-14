ba19cd3500afa9db345017f9a0ca79714bc5ef88
Evan Ryan <Evan Ryan>
2017-03-13 23:39:26 -0400

    another

--- server.js
+++ server.js
@@ -5,42 +5,42 @@ var http = require('http')
 var server = http.createServer(function (request, response) {
   if (request.url === '/') {
     response.end(
-      '<h1>HOME</h1>'
-      + '<p>HOW ARE YOU THIS FINE DAY?</p>'
-      + '<a href="/cuteness">Click here for adorable animals</a>'
-      + '<a href="/random-joke">Click here for hilarious jokes</a>')
+      '<h1>HOME</h1>' + '<p>HOW ARE YOU THIS FINE DAY?</p>' + '<a href="/cuteness">Click here for adorable animals</a>' +
+      '<a href="/random-joke">Click here for hilarious jokes</a>')
   } else if (request.url === '/cuteness') {
     response.end(
-      '<h1>CUTE ANIMALS</h1>'
-      + '<a href="/">TO THE HOME PAGE</a>'
-      + '<img src="http://i.dailymail.co.uk/i/pix/2013/07/03/article-0-1AA2F04F000005DC-736_638x415.jpg" alt="CUTE">')
+      '<h1>CUTE ANIMALS</h1>' +
+       '<a href="/">TO THE HOME PAGE</a>' +
+       '<img src="http://i.dailymail.co.uk/i/pix/2013/07/03/article-0-1AA2F04F000005DC-736_638x415.jpg" alt="CUTE">')
   } else if (request.url === '/random-joke') {
     var jokes = Math.random()
     if (jokes > 0.66) {
-      var one = '<p>knock knock</p>'
-      + '<p>who\'s there?</p>'
-      + '<p>An extraterrestrial.</p>'
-      + '<p>Extraterrestrial who?</p>' + '<p>What - how many extra-terrestrials do you know?</p>'
+      var one = '<p>knock knock</p>' +
+       '<p>who\'s there?</p>' +
+       '<p>An extraterrestrial.</p>' +
+       '<p>Extraterrestrial who?</p>' +
+       '<p>What - how many extra-terrestrials do you know?</p>'
   } else if (jokes < 0.33) {
-      var one = '<p>Knock Knock</p>'
-      + '<p>Who\'s there?</p>'
-      + '<p>a broken pencil</p>'
-      + '<p>a broken pencil who?</p>'
-      + '<p>nevermind its pointless.</p>'
-  } else { var one = '<p>knock knock</p>'
-      + '<p>who\'s there?</p>'
-      + '<p>Amos</p>'
-      + '<p>Amos who</p>'
-      + '<p>a mosquito</p>' }
+      var one = '<p>Knock Knock</p>' +
+      '<p>Who\'s there?</p>' +
+       '<p>a broken pencil</p>' +
+       '<p>a broken pencil who?</p>' +
+       '<p>nevermind its pointless.</p>'
+  } else {
+      var one = '<p>knock knock</p>' +
+       '<p>who\'s there?</p>' +
+       '<p>Amos</p>' +
+       '<p>Amos who</p>' +
+       '<p>a mosquito</p>' }
 
     response.end('<h1>HILARIOUS RANDOM JOKES</h1>' + '<a href="/">TO THE HOME PAGE</a>' + one)
-  } else (request.url = '/lostloveandregrets')
+  } else {
   response.end(
-    '<h1>This is not the page you are looking for</h1>'
-    + '<a href="/">TO THE HOME PAGE</a>'
-    + '<img src="https://s-media-cache-ak0.pinimg.com/originals/f8/c3/e8/f8c3e844d98cb3ca75a58806e11fe808.png" alt="goodhope">'
-    + '<a href="/">TO THE HOME PAGE</a>')
-})
+    '<h1>This is not the page you are looking for</h1>' +
+     '<a href="/">TO THE HOME PAGE</a>' +
+     '<img src="https://s-media-cache-ak0.pinimg.com/originals/f8/c3/e8/f8c3e844d98cb3ca75a58806e11fe808.png" alt="goodhope">' +
+     '<a href="/">TO THE HOME PAGE</a>' + request.url)
+}})
 // Listen on port 8080, so that we can reach the app at
 // localhost:8080
 
